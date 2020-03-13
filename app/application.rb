cc40f368866ed5def5c881ce1f9ab0fc25d2352b
Stela Capsa <stela.capsa@gmail.com>
2020-03-13 02:30:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,21 +5,20 @@ class Application
     req = Rack::Request.new(env)
     
     
-    if req.path=="/item"
-      items = @@item.map do |i|
-        i.name == item_name
-        if item
-      resp.write item.price
+    if req.path.match(/items\/.+/)
+      item_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
-      resp.write "Item not found"
-      resp.status = 400 
+      resp.write "Route not found"
+      resp.status = 404
     end
-  end 
-    
-    else
-       resp.write "Route not found"
-       resp.status = 404 
-     end 
+
     resp.finish
   end
 end
