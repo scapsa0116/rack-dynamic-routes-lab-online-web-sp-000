af5e0bdddc7532125b2c400779692e8c5debba6b
Stela Capsa <stela.capsa@gmail.com>
2020-03-13 02:34:03 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,24 @@
+class Application
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    
+    if req.path.match(/items\/.+/)
+      i_name = req.path.split("/items/").last
+      item = @@items.detect { |i| i.name == i_name }
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+end
