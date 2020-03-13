e025df457534e354461338a08a0c4914e1e1f3bc
Stela Capsa <stela.capsa@gmail.com>
2020-03-13 02:29:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,11 +14,12 @@ class Application
       resp.write "Item not found"
       resp.status = 400 
     end
+  end 
     
     else
        resp.write "Route not found"
        resp.status = 404 
- 
+     end 
     resp.finish
   end
 end
