3297d2849ec4dd824a1c62cf2cb185d84ff0c667
Stela Capsa <stela.capsa@gmail.com>
2020-03-13 02:31:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
     
     
-    if req.path == (/items\/.+/)
+    if req.path.match(/items\/.+/)
       item_name = req.path.split("/items/").last
       item = @@items.detect { |i| i.name == item_name }
       if item
